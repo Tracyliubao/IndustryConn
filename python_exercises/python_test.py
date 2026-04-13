0f65509fc0284d89322a38175a6e91ed88c9e8bd
liubao <liubao_android@qq.com>
2026-04-13 20:51:34 +0800

    new:完成Python基础语法练习 Day1-3

---
+++ python_exercises/python_test.py
@@ -0,0 +1,61 @@
+# 这是一条注释
+# print("hello world")
+# print('"Hello, World"')
+# print('she doesn\'t is my father')
+# print(r'C:\this\name') # 引号前加r，可以避免转义
+# print('''\
+# Usage: thingy [OPTIONS]
+# 	 -h                        Display this usage message
+# 	 -H hostname 
+# ''')
+# addr = 3
+# noformat = 'adf adf adf'
+# print(f'{addr}father')
+# print(str(3) + 'father')
+# print(str.format(noformat))
+# string = 'python'
+# print(string[-5]) #y
+# print(string[:2]) #py
+# print(string[2:]) #thon
+
+#实战
+# #day1
+# address = input()
+# print(f'正在连接设备地址{address}')
+# com1 = 1
+# com2 = 3
+# print(f'{com1:02X}{com2:02X}')
+# num = 258
+# print(f'{num:04X}')
+
+# list = [1,2,3,4,5]
+# list2 = list[:2]
+# print(list)
+# print(list2)
+
+# #day2
+# temperatures = [25, 31, 28, 33, 27]
+# for x in temperatures:
+# 	if (x > 30):
+# 		print(x)
+# commend = '01030201023815'
+# print(commend[:-4])
+# com = int(input())
+# pote = [9600, 19200, 115200]
+# if com in pote:
+# 	print('波特率正确')
+# else:
+# 	print('波特率错误')
+
+#day3
+def hex_to_int(hex_str):
+	return int(hex_str, 16)#直接将16进制转成10进制
+def build_cmd(addr, func):
+	return f'{addr:02X}{func:02X}'
+def extract_data(commend):
+	return commend[0:-4]
+print(hex_to_int('0102'))
+print(build_cmd(1,3))
+print(extract_data("01030201023815"))
+
+
