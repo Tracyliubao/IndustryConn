6e83cd3bfe31e343b63dd7da4d0158742b23b999
liubao <liubao_android@qq.com>
2026-04-17 22:15:25 +0800

    modify:完成QoS 0与QoS 1消息发布接收对比实验

--- python_exercises/mqtt_test.py
+++ python_exercises/mqtt_test.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
 
-received_message = None
+received_message = []
 subscribed = False  # 订阅完成标志
 
 # 5. “连接成功”自动调用
@@ -11,7 +11,7 @@ def on_connect(client, userdata, flags, rc):
     # 7. rc=0 代表成功，非0代表失败（像 HTTP 状态码）
     print(f"已连接至 HiveMQ Broker，返回码: {rc}")
     # 订阅主题
-    client.subscribe("liubao/test")
+    client.subscribe("liubao/test", qos = 1)
     # 标记订阅请求已发出（实际在 Broker 侧几乎即时生效）
     # 9. 订阅请求已发出，把标志位设成 True
     subscribed = True  
@@ -23,9 +23,12 @@ def on_message(client, userdata, msg):
     # 11. 告诉 Python：我要修改外面那个全局变量 received_message
     global received_message
     # 12. 把收到的字节数据转成字符串，存起来
-    received_message = msg.payload.decode()
+    content = msg.payload.decode()
+    # 获取消息的QoS等级
+    qos = msg.qos
     # 13. 打印主题和内容
-    print(f"收到消息: {msg.topic} -> {received_message}")
+    print(f"收到消息: QoS = {qos}, topic = {msg.topic}, content = {content}")
+    received_message.append(content)
 
 # 14. 创建一个 MQTT 客户端实例
 client = mqtt.Client()
@@ -54,19 +57,24 @@ if not subscribed:
 time.sleep(0.5)
 
 # 现在发布消息
-client.publish("liubao/test", "订阅确认后再发布，必达！")
-print("已发布消息，等待接收...")
+print('发布QoS 0 消息。。。')
+client.publish("liubao/test", "这条消息可能丢失", qos = 0) #只发一次
+time.sleep(1)
 
-# 等待接收消息
+print("发布QoS 1 消息。。。")
+client.publish("liubao/test", "这条消息保证到达", qos = 1) #至少发一次，确保对方收到
 time.sleep(3)
 
 client.loop_stop()
 client.disconnect()
 
-if received_message:
-    print(f"【成功】接收到的消息: {received_message}")
+print(f"总共收到{len(received_message)}条消息")
+if len(received_message) == 2:
+    print(f"【成功】收到两条消息")
+elif len(received_message) == 1:
+    print(f"【成功】收到一条消息")
 else:
-    print("【失败】仍未收到消息，请稍后重试。")
+    print("【失败】两条消息都没收到")
 
 #nc -vz mqtt.eclipseprojects.io 1883
 #nc -vz broker.hivemq.com 1883
