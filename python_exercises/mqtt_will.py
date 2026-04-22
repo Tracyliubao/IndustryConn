765714e007e3ec894f29195a81ed06d7e22fe1fd
liubao <liubao_android@qq.com>
2026-04-22 22:08:12 +0800

    new:新增mqtt连接失败，将数据保存到本地模拟功能

--- python_exercises/mqtt_will.py
+++ python_exercises/mqtt_will.py
@@ -4,7 +4,6 @@ import logging
 
 def on_connect(client, userdata, flags, rc):
 	print(f"已连接，返回码是{rc}")
-	client.publish("liubao/will", "online", qos = 1, retain = True)
 
 def on_message(client, userdata, msg):
 	print(f"收到消息：{msg.topic} -> {msg.payload.decode()}")
@@ -21,6 +20,9 @@ def connect_with_retry(client, host, port, max = 3):
 			time.sleep(2) #每2秒重连一次
 
 	return False
+# 模拟存入数据库
+def savetodatabase(data):
+	print(data)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -31,24 +33,25 @@ client.on_message = on_message
 # retain表示会保留消息，新订阅者上线也能立即看到状态
 client.will_set(topic = "liubao/will", payload = "offline", qos = 1, retain = True)
 
-if connect_with_retry(client, "broker.hivemq.com", 1883):
+if connect_with_retry(client, "broker.hivemqx.com", 1883):
 	client.loop_start()
 	# 等待异步连接完成
 	time.sleep(2)
 
-	# 模拟正在工作
-	print("设备正在工作中。。。")
-	time.sleep(10)
-	
-	client.publish("liubao/will", "offline_normal", qos = 1)
-	print("正常下线")
-	time.sleep(1)
+	value = [258] #实际用read_register
+	if value:
+		topic = "liubao/will"
+		payload = str(value[0])
+		client.publish(topic, payload, qos = 1)
 	
 	client.loop_stop()
 	client.disconnect()
 else :
-	print("连接失败")
-	exit(1)
+	value = [258]
+	if value:
+		savetodatabase(str(value[0]))
+	print("MQTT 连接失败，数据暂存本地，程序退出")
+	exit(0)
 
 
 
