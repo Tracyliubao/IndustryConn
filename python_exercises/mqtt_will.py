e5bfdacb04885573701b0f7ec59859a3d4650e81
liubao <liubao_android@qq.com>
2026-04-24 21:53:40 +0800

    new:新增mqtt单条补传机制和无限重试机制

--- python_exercises/mqtt_will.py
+++ python_exercises/mqtt_will.py
@@ -1,6 +1,9 @@
 import time
 import paho.mqtt.client as mqtt
 import logging
+from modbus_tool import read_register
+
+MAX_PUBLISH_RETRIES = 2
 
 def on_connect(client, userdata, flags, rc):
 	print(f"已连接，返回码是{rc}")
@@ -24,6 +27,14 @@ def connect_with_retry(client, host, port, max = 3):
 def savetodatabase(data):
 	print(data)
 
+def query_data_size():
+	list = select * from table
+	return list.size
+
+def query_data():
+	list = select * from table
+	return list
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -33,25 +44,47 @@ client.on_message = on_message
 # retain表示会保留消息，新订阅者上线也能立即看到状态
 client.will_set(topic = "liubao/will", payload = "offline", qos = 1, retain = True)
 
-if connect_with_retry(client, "broker.hivemqx.com", 1883):
-	client.loop_start()
-	# 等待异步连接完成
-	time.sleep(2)
+while True:
+	if connect_with_retry(client, "broker.hivemqx.com", 1883):
+		client.loop_start()
+		# 等待异步连接完成
+		time.sleep(2)
 
-	value = [258] #实际用read_register
-	if value:
-		topic = "liubao/will"
-		payload = str(value[0])
-		client.publish(topic, payload, qos = 1)
-	
-	client.loop_stop()
-	client.disconnect()
-else :
-	value = [258]
-	if value:
-		savetodatabase(str(value[0]))
-	print("MQTT 连接失败，数据暂存本地，程序退出")
-	exit(0)
+		pending_rows = query_data
+		for row in pending_rows:
+			topic, payload, qos = row
+			success = False
+			# 单条补传机制
+			for temp in range(MAX_PUBLISH_RETRIES + 1):
+				try:
+					client.publish(topic, payload, qos)
+					success = True
+					break
+				except Exception as e:
+					if temp < MAX_PUBLISH_RETRIES:
+						# 补传加间隔控制
+						time.sleep(0.1)
+				
+			if success:
+				delete_pending(id)
+			else :
+				break
+
+
+		while True:
+			try:
+				value = read_register()
+				if value:
+					client.publish("liubao/will", str(value[0]), qos = 1)
+				time.sleep(1)
+			except Exception as e:
+				break
+	else :
+		value = read_register()
+		if value:
+			savetodatabase(str(value[0]))
+		print("MQTT 连接失败，数据暂存本地，程序退出")
+		time.sleep(10)
 
 
 
